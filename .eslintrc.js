41e8cb38b9e8b19059b1a91eeb41aae74fd0721a
Brady Wied <brady@bswtechconsulting.com>
2016-04-05 22:31:59 -0600

    add linting

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "rules": {
+        "no-console": 0,
+        "indent": ["error", 2],
+        "quotes": [
+            2,
+            "single"
+        ],
+        "linebreak-style": [
+            2,
+            "unix"
+        ],
+        "semi": [
+            2,
+            "never"
+        ]
+    },
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "globals": {
+        "Opal": true
+    },
+    "extends": "eslint:recommended"
+};
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-'use strict';
+'use strict'
 
-require("./vendor/opal-compiler.js")
+require('./vendor/opal-compiler.js')
 const loaderUtils = require('loader-utils')
 const sourceMap = require('source-map')
 const SourceNode = sourceMap.SourceNode
@@ -13,19 +13,19 @@ const cache = require('./lib/fs-cache.js')
 const queryString = require('querystring')
 
 const opalVersion = Opal.get('RUBY_ENGINE_VERSION')
-const LOAD_PATH = process.env.OPAL_LOAD_PATH ? process.env.OPAL_LOAD_PATH.split(":") : [process.cwd()];
+const LOAD_PATH = process.env.OPAL_LOAD_PATH ? process.env.OPAL_LOAD_PATH.split(':') : [process.cwd()]
 
 if (LOAD_PATH.length === 0) {
-  console.warn("OPAL_LOAD_PATH environment variable is not set")
-  console.warn("By default, loader will only load from path relative to current source")
+  console.warn('OPAL_LOAD_PATH environment variable is not set')
+  console.warn('By default, loader will only load from path relative to current source')
 }
 
 function getCurrentLoader(loaderContext) {
-  return loaderContext.loaders[loaderContext.loaderIndex];
+  return loaderContext.loaders[loaderContext.loaderIndex]
 }
 
 function resolveFilename(filename) {
-  let rubyFileName = filename.replace(/(\.rb)?$/, ".rb");
+  let rubyFileName = filename.replace(/(\.rb)?$/, '.rb')
 
   // FIXME
   // Workaround to make "require 'opal'" work, original opal will try to concate raw js
@@ -33,23 +33,23 @@ function resolveFilename(filename) {
     rubyFileName = 'corelib/runtime.js'
   }
 
-  let result = null;
-  // opal only looks  @ the load path, so that's what we'll do
+  let result = null
+    // opal only looks  @ the load path, so that's what we'll do
   for (var dir of LOAD_PATH) {
-    let fullPath = path.resolve(dir, rubyFileName);
+    let fullPath = path.resolve(dir, rubyFileName)
     if (fs.existsSync(fullPath)) {
       result = {
         absolute: fullPath,
         relative: path.relative(dir, rubyFileName)
       }
-      break;
+      break
     }
   }
 
   if (result) {
-    return result;
+    return result
   } else {
-    throw new Error(`Cannot load file - ${filename}`);
+    throw new Error(`Cannot load file - ${filename}`)
   }
 }
 
@@ -57,51 +57,51 @@ function getCompiler(source, options) {
   // it's important to not give an absolute path to Opal (only relative to load path)
   // otherwise absolute paths end up in the compiled code
   const relativePath = options.relativeFileName
-  // don't want Opal.modules to have an extension
+    // don't want Opal.modules to have an extension
   const withoutExtension = relativePath.replace(path.extname(relativePath), '')
   const compilerOptions = Object.assign({
-    file: withoutExtension, // opal calls it file
-  }, options);
+    file: withoutExtension // opal calls it file
+  }, options)
   delete compilerOptions.filename
-  return Opal.Opal.Compiler.$new(source, Opal.hash(compilerOptions));
+  return Opal.Opal.Compiler.$new(source, Opal.hash(compilerOptions))
 }
 
 function processSourceMaps(compiler, source, resourcePath, rawResult, prepend) {
-  let rawMap = JSON.parse(compiler.$source_map().$as_json().$to_json());
+  let rawMap = JSON.parse(compiler.$source_map().$as_json().$to_json())
 
   // Since it's compiled from the current resource
-  rawMap.sources = [resourcePath];
+  rawMap.sources = [resourcePath]
 
   // Set source content
   let consumer = new SourceMapConsumer(rawMap)
-  let map = SourceMapGenerator.fromSourceMap(consumer);
-  map.setSourceContent(resourcePath, source);
+  let map = SourceMapGenerator.fromSourceMap(consumer)
+  map.setSourceContent(resourcePath, source)
 
   // Prepend the chunk of our injected script
-  let node = SourceNode.fromStringWithSourceMap(rawResult, new SourceMapConsumer(map.toString()));
-  node.prepend(prepend.join(" "));
+  let node = SourceNode.fromStringWithSourceMap(rawResult, new SourceMapConsumer(map.toString()))
+  node.prepend(prepend.join(' '))
   return JSON.parse(node.toStringWithSourceMap().map.toString())
 }
 
 function transpile(source, options) {
   const compiler = getCompiler(source, options)
 
-  compiler.$compile();
+  compiler.$compile()
 
-  const result = compiler.$result();
+  const result = compiler.$result()
 
   /*
     Workaround to make IO work,
     webpack polyfill global "process" module by default,
     while Opal::IO rely on it to deterimine in node environment or not
   */
-  let prepend = ['process = undefined;'];
+  let prepend = ['process = undefined;']
 
   const addRequires = files => {
     files.forEach(filename => {
-      var resolved = resolveFilename(filename).absolute;
+      var resolved = resolveFilename(filename).absolute
       if (resolved.match(/\.js$/)) {
-        prepend.push(`require('${require.resolve('imports-loader')}!${resolved}');`);
+        prepend.push(`require('${require.resolve('imports-loader')}!${resolved}');`)
         prepend.push(`Opal.loaded('${filename}');`)
       } else {
         const passOnOptions = Object.assign({}, options, {
@@ -114,26 +114,28 @@ function transpile(source, options) {
         delete passOnOptions.sourceMap
         delete passOnOptions.relativeFileName
         const flat = queryString.stringify(passOnOptions)
-        prepend.push(`require('!!${options.currentLoader}?${flat}!${resolved}');`);
+        prepend.push(`require('!!${options.currentLoader}?${flat}!${resolved}');`)
       }
     })
   }
 
   addRequires(compiler.$requires())
 
-  compiler.$required_trees().forEach(function (dirname) {
+  compiler.$required_trees().forEach(function(dirname) {
     // path will only be relative to the file we're processing
     let resolved = path.resolve(options.filename, '..', dirname)
-    // TODO: Look into making this async
+      // TODO: Look into making this async
     let files = fs.readdirSync(resolved)
     let withPath = []
-    // fs.readdir only returns the filenames, not the base directory
-    files.forEach(function (filename) { withPath.push(path.join(dirname, filename)) })
+      // fs.readdir only returns the filenames, not the base directory
+    files.forEach(function(filename) {
+      withPath.push(path.join(dirname, filename))
+    })
     addRequires(withPath)
   })
 
   let response = {
-    code: prepend.join(" ") + "\n" + result
+    code: prepend.join(' ') + '\n' + result
   }
   if (options.sourceMap) {
     response.map = processSourceMaps(compiler, source, options.filename, result, prepend)
@@ -144,11 +146,11 @@ function transpile(source, options) {
 module.exports = function(source) {
   var result = {}
 
-  const webpackRemainingChain = loaderUtils.getRemainingRequest(this).split('!');
-  const filename = webpackRemainingChain[webpackRemainingChain.length - 1];
-  const globalOptions = this.options.opal || {};
-  const loaderOptions = loaderUtils.parseQuery(this.query);
-  const userOptions = Object.assign({}, globalOptions, loaderOptions);
+  const webpackRemainingChain = loaderUtils.getRemainingRequest(this).split('!')
+  const filename = webpackRemainingChain[webpackRemainingChain.length - 1]
+  const globalOptions = this.options.opal || {}
+  const loaderOptions = loaderUtils.parseQuery(this.query)
+  const userOptions = Object.assign({}, globalOptions, loaderOptions)
   const relativeFileName = resolveFilename(filename).relative
   const defaultOptions = {
     sourceRoot: process.cwd(),
@@ -158,8 +160,8 @@ module.exports = function(source) {
     cacheIdentifier: JSON.stringify({
       'opal-loader': pkg.version,
       'opal-compiler': opalVersion,
-      env: process.env.OPAL_ENV || process.env.NODE_ENV,
-    }),
+      env: process.env.OPAL_ENV || process.env.NODE_ENV
+    })
   }
   const options = Object.assign({}, defaultOptions, userOptions)
 
@@ -173,18 +175,20 @@ module.exports = function(source) {
   this.cacheable()
 
   if (cacheDirectory) {
-    var callback = this.async();
+    var callback = this.async()
     return cache({
       directory: cacheDirectory,
       identifier: cacheIdentifier,
       source: source,
       options: options,
-      transform: transpile,
+      transform: transpile
     }, function(err, result) {
-      if (err) { return callback(err); }
-      return callback(null, result.code, result.map);
-    });
+      if (err) {
+        return callback(err)
+      }
+      return callback(null, result.code, result.map)
+    })
   }
-  result = transpile(source, options);
-  this.callback(null, result.code, result.map);
-};
+  result = transpile(source, options)
+  this.callback(null, result.code, result.map)
+}
