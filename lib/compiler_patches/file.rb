f8c2e2bc25ab6870026f3c5b84ea798a1a480228
Brady Wied <brady@bswtechconsulting.com>
2016-04-19 10:49:07 -0600

    Backport File::Stat, File::exist? changes, File::new, File::expand_path from 0.10

--- lib/compiler_patches/file.rb
+++ lib/compiler_patches/file.rb
@@ -1,9 +1,46 @@
-# Backports from opal 0.10, in O.9, dirname comes back as an array for this case
-# https://github.com/opal/opal/pull/1429
-result = File.dirname('stuff')
-unless result.is_a?(String) && result == '.' && File.method_defined?(:basename) && File.method_defined?(:extname)
-  class File
+class File
+  # Backports from opal 0.10, in O.9, dirname comes back as an array for this case
+  # https://github.com/opal/opal/pull/1429
+  result = File.dirname('stuff')
+  unless result.is_a?(String) && result == '.' && File.method_defined?(:basename) && File.method_defined?(:extname)
+    @__fs__ = node_require :fs
+    `var __fs__ = #{@__fs__}`
+
+    # https://github.com/opal/opal/commit/a0d07f82ffc745e208a509c28a415b914af8c435
+    def initialize(path, flags)
+      binary_flag_regexp = /b/
+      encoding_flag_regexp = /:(.*)/
+      # binary flag is unsupported
+      warn "Binary flag (b) is unsupported by Node.js openSync method, removing flag." if flags.match(binary_flag_regexp)
+      flags = flags.gsub(binary_flag_regexp, '')
+      # encoding flag is unsupported
+      warn "Encoding flag (:encoding) is unsupported by Node.js openSync method, removing flag." if flags.match(encoding_flag_regexp)
+      flags = flags.gsub(encoding_flag_regexp, '')
+      @path = path
+      @flags = flags
+      @fd = `__fs__.openSync(path, flags)`
+    end
+
     class << self
+      def expand_path(path, basedir = nil)
+        path = [basedir, path].compact.join(SEPARATOR)
+        parts = path.split(SEPARATOR)
+        new_parts = []
+        parts[0] = Dir.home if parts.first == '~'
+        parts[0] = Dir.pwd if parts.first == '.'
+
+        parts.each do |part|
+          if part == '..'
+            new_parts.pop
+          else
+            new_parts << part
+          end
+        end
+        new_parts.join(SEPARATOR)
+      end
+
+      alias realpath expand_path
+
       %x{
         function chompdirsep(path) {
           var last;
@@ -214,13 +251,39 @@ unless result.is_a?(String) && result == '.' && File.method_defined?(:basename) 
         # extension name must contains at least one character .(something)
         (last_dot_idx.nil? || last_dot_idx + 1 == filename.length - 1) ? '' : filename[(last_dot_idx + 1)..-1]
       end
+
+      def exist? path
+        path = path.path if path.respond_to? :path
+        `__fs__.existsSync(#{path})`
+      end
     end
+  end
+
+  # probably the only thing in here that's not yet a backport from 0.10
+  unless File.const_defined?('FNM_SYSCASE')
+    # case insenstive filesysem??
+    FNM_SYSCASE = 0
   end
 end
 
-unless File.const_defined?('FNM_SYSCASE')
+unless File.const_defined?('Stat')
+  class File::Stat
+    @__fs__ = node_require :fs
+    `var __fs__ = #{@__fs__}`
+
+    def initialize(path)
+      @path = path
+    end
+
+    def file?
+      `__fs__.statSync(#{@path}).isFile()`
+    end
+  end
+
   class File
-    # case insenstive filesysem??
-    FNM_SYSCASE = 0
+    def self.stat path
+      path = path.path if path.respond_to? :path
+      File::Stat.new(path)
+    end
   end
 end
